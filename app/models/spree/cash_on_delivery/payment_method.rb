a566fff891b3f4ded1c7fc090e0991806d9a9dfa
ror <mister-al@ya.ru>
2012-06-14 11:54:17 +0600

    сделал из другого

--- app/models/spree/cash_on_delivery/payment_method.rb
+++ app/models/spree/cash_on_delivery/payment_method.rb
@@ -1,15 +1,57 @@
 module Spree
   class CashOnDelivery::PaymentMethod < Spree::PaymentMethod
+
     def payment_profiles_supported?
       true # we want to show the confirm step.
     end
 
-    def provider_class
-      self.class
+    def post_create(payment)
+      payment.order.adjustments.each { |a| a.destroy if a.originator == payment }
+      payment.order.adjustments.create(:amount => Spree::Config[:cash_on_delivery_charge],
+                               :source => payment,
+                               :originator => payment,
+                               :label => I18n.t(:shipping_and_handling))
     end
+    
+    def update_adjustment(adjustment, src)
+      adjustment.update_attribute_without_callbacks(:amount, Spree::Config[:cash_on_delivery_charge])
+    end
+
+
+    def authorize(*args)
+      ActiveMerchant::Billing::Response.new(true, "", {}, {})
+    end
+
+    def capture(payment, source, gateway_options)
+      ActiveMerchant::Billing::Response.new(true, "", {}, {})
+    end
+
+    def void(*args)
+      ActiveMerchant::Billing::Response.new(true, "", {}, {})
+    end
+
+    def actions
+      %w{capture void}
+    end
+
+    def can_capture?(payment)
+      payment.state == 'pending' || payment.state == 'checkout'
+    end
+    
+    def can_void?(payment)
+      payment.state != 'void'
+    end
+
+    def source_required?
+      false
+    end
+
+    #def provider_class
+    #  self.class
+    #end
   
     def payment_source_class
-      Spree::CashOnDelivery::Transaction
+      nil
     end
   
     def method_type
